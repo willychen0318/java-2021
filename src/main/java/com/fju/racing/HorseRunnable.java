98be45cb0bc4759cd7cdf731fd792d9cdbafc891
willychen0318 <willy03180201@gmail.com>
2021-04-13 16:23:24 +0800

    Thread exam exercise

--- src/main/java/com/fju/racing/HorseRunnable.java
+++ src/main/java/com/fju/racing/HorseRunnable.java
@@ -5,7 +5,7 @@ public class HorseRunnable implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < 10000; i++) {
-            System.out.println("RR" + ""+i);
+            System.out.println("RR" +" "+i);
         }
     }
 }
--- src/main/java/com/fju/racing/Racing.java
+++ src/main/java/com/fju/racing/Racing.java
@@ -1,19 +1,18 @@
 package com.fju.racing;
-//Thread exercise
+
 public class Racing {
     public static void main(String[] args) {
-        Horse h1= new Horse();//free constructor
-        h1.start();//not h1.run()
-
-        /*for (int i = 0; i < 10000; i++) {
-            System.out.println("Horse1"+" "+i);
-        }main method just work this for(), h2 work itself (Thread-0)*/
-        Horse h2= new Horse();
+        Horse h2 =new Horse();
         h2.start();
+        Horse h1 =new Horse();
+        h1.start();
         HorseRunnable h3=new HorseRunnable();
         Thread thread=new Thread(h3);
         thread.start();
+
     }
-
-
 }
+//First,write two horse racing ,then write Horse method extends Thread .
+//Second,use Horse h2 to racing with Horse1
+//Third,use Horse change Horse1 to h1.
+//Final, write HorseRunnable to write h3 in Racing.
