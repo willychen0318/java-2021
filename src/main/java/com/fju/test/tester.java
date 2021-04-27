fbe9f700f11df19fcad28f751cb3f39b6ef33b12
WillyChen <willy03180201@gmail.com>
2021-04-27 16:26:48 +0800

    Box polymorphism and List preview

--- src/main/java/com/fju/test/tester.java
+++ src/main/java/com/fju/test/tester.java
@@ -3,7 +3,10 @@ package com.fju.test;
 public class tester {
     public static void main(String[] args) {
         //Box1 box1=new Box1();
-        //Box2 box2=new Box2(); bad box
+        //Box2 box2=new Box2(); =>bad box
+        //leftBox2 <=> rightBox2() but in this array ,Box[] not equal to Box1(), Box2(),.....
+        //Because in java,parent's datatype can equal to child's datatype(int, double.....)
+        //This is java polymorphism.
         Box[] boxes={new Box1(), new Box2(), new Box3(), new Box4(), new Box5(), new Box6()};
         int length=7;
         int width=5;
@@ -14,6 +17,11 @@ public class tester {
         }else....*/
 
         for (Box box:boxes) {
+            //Box1 box1=(Box1) box;
+            //Box[0]=new Box1();
+            //Box[1]=new Box2();.......
+            //If Box is not be defined,it will not default 0 but Null.
+            //Ex=>System.out.println(Box[3].name);=>Exception
             if (box.validate(length, width, height)) {
                 System.out.println(box.name+" it is!");
                 break;
