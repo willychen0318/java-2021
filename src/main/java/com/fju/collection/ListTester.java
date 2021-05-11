3de97478cfa8fda52b925aa27ad4f2e5acdc6f6c
willychen <willy03180201@gmail.com>
2021-05-11 15:50:38 +0800

    Collection Set

--- src/main/java/com/fju/collection/ListTester.java
+++ src/main/java/com/fju/collection/ListTester.java
@@ -5,9 +5,23 @@ import com.fju.test.Box1;
 import com.fju.test.Box2;
 
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class ListTester {
     public static void main(String[] args) {
+        //Set non-repeat
+        TreeSet <String>aa=new TreeSet<String>();
+        System.out.println(aa.size());
+        aa.add("Hank");
+        aa.add("Tom");
+        aa.add("Tom");
+        aa.add("Ken");
+        System.out.println(aa.size());
+        for (String a : aa) {
+            System.out.println(a);
+        }//Set iterator use for each
+
+
         int[] num=new int[5];
         num[0]=1;
         //num[1]=true [X]
@@ -21,13 +35,13 @@ public class ListTester {
         //Integer n=123;
         list.add(n);
         //Float, Byte, Double, Long, Short, Character, Boolean, Integer
-        list.add(123);//Boxing=>Java let 123=>int then put int into add.
+        list.add(1234);//Boxing=>Java let 123=>int then put int into add.
 
         list.add(true);
         Box1 box=new Box1();
         list.add(box);
         String name=(String)list.get(0);
-        String number=(String)list.get(1);
+        //String number=(String)list.get(1);
         //Although java.list can do "Heterogeneous", but it's useless because if you want to
         //take element ,it have to trans ,So we can limit list's datatype.
         Box1 box1=new Box1();
