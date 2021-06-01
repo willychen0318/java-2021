2ea735dd14922dc5af7634a83bcf7ffaab56ab88
willychen0318 <willy03180201@gmail.com>
2021-06-01 15:04:52 +0800

    use JFrame to write a parking frame

---
+++ src/main/java/com/fju/car/CarWin.java
@@ -0,0 +1,14 @@
+package com.fju.car;
+
+import javax.swing.*;
+
+public class CarWin {
+    static final int width=600;
+    static final int height=400;
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setSize(width,height);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
